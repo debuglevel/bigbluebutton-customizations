3d7dd1e3136c33f602f1115a9b3a37c2ef2692f0
Marc Kohaupt <debuglevel@gmail.com>
2021-01-26 21:13:01 +0100

    Adding webcam profiles to meteor config should basically work

--- 008 generate webcam profiles/set-meteor-configuration.sh
+++ 008 generate webcam profiles/set-meteor-configuration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Setting webcam settings"
+echo "Setting webcam settings..."
 
-# TODO: apply-lib.sh must be sourced ot something to get $HTML5_CONFIG
+# TODO: apply-lib.sh must be sourced or something to get $HTML5_CONFIG
 
 HTML5_CONFIG="test.yml"
 
-# WebRTC allows minimum of 30kbit/s (says BBB docs)
+# WebRTC allows a minimum of 30kbit/s (says BBB docs)
 BITRATE_MIN=30
 BITRATE_MAX=200
 BITRATE_STEP=30
@@ -13,20 +13,29 @@ BITRATE_STEP=30
 FRAMERATE_MIN=1
 FRAMERATE_MAX=15
 FRAMERATE_STEP=2
-for bitrate in $(seq $BITRATE_MIN $BITRATE_STEP $BITRATE_MAX); do
-  #echo "Bitrate $bitrate"
-  for framerate in `seq $FRAMERATE_MIN $FRAMERATE_STEP $FRAMERATE_MAX`; do
-    #echo "Framerate $framerate"
 
-    id="custom-r$framerate-b$bandwidth"
-    name="Custom: $framerate fps, $bandwidth kbit/s"
-    echo "$id: $bandwidth kbit/s, $framerate fps"
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).id" $id
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).name" $name
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).bitrate" $bitrate
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).default" false
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).hidden" false
-    yq w -i $HTML5_CONFIG "public.kurento.cameraProfiles.(id==$id).constraints.frameRate" $framerate
+for bitrate in $(seq $BITRATE_MIN $BITRATE_STEP $BITRATE_MAX); do
+  for framerate in `seq $FRAMERATE_MIN $FRAMERATE_STEP $FRAMERATE_MAX`; do
+    id="custom-r$framerate-b$bitrate"
+    name="Custom: $framerate fps, $bitrate kbit/s"
+    echo "Processing profile id=$id: $bitrate kbit/s, $framerate fps"
+
+    time=$(date)
+    dict="{\"id\": \"$id\", \"name\": \"$name\", \"bitrate\": \"$bitrate\", \"default\": false, \"hidden\": false, \"constraints\": { \"frameRate\": $framerate }, \"updated-on\": \"$time\" }"
+
+    # check if exists
+    echo "Checking if profile id=$id exists..."
+    OUTPUT=$(yq4 eval "(.public.kurento.cameraProfiles.[] | select(.id == \"$id\")) | not" test.yml)
+    #echo Output- $OUTPUT -x
+    # if exists -> false
+    # if not exists -> no output
+    if [[ "$OUTPUT" == "false" ]]; then
+      echo "Profile id=$id exists, updating..."
+      yq4 -i eval "(.public.kurento.cameraProfiles.[] | select(.id == \"$id\")) = $dict" $HTML5_CONFIG
+    else
+      echo "Profile id=$id does not exist, adding..."
+      yq4 -i eval ".public.kurento.cameraProfiles += $dict" $HTML5_CONFIG
+    fi
   done
 done
 
