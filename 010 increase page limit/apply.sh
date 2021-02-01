00d254a22cbde02c0398dae3e5a34f8ac44b6c45
root <root@bbb-testing.avm-konferenz.de>
2021-02-01 11:14:23 +0100

    Add debug messages

--- 010 increase page limit/apply.sh
+++ 010 increase page limit/apply.sh
@@ -1,6 +1,6 @@
 # https://docs.bigbluebutton.org/2.2/customize.html#increase-the-200-page-limit-for-uploads
-echo "UNTESTED"
-return
+BBB_CONF="/usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties"
 
-echo "Setting page limit..."
-sed -i 's/maxNumPages=.*/maxNumPages=500/g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+echo "- Setting page limit..."
+echo "  - Setting value in $BBB_CONF via sed"
+sed -i 's/maxNumPages=.*/maxNumPages=500/g' $BBB_CONF
